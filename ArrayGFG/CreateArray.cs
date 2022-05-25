5dea0c4b4864e0bc558972aeecc718db1975674b
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-05-25 11:56:02 +0530

    Add project files.

---
+++ ArrayGFG/CreateArray.cs
@@ -0,0 +1,19 @@
+﻿namespace ArrayGFG
+{
+    public class CreateArray
+    {
+        public void Execute()
+        {
+            int[] numArr = new int[5];
+            numArr[0] = 1;
+            numArr[1] = 2;
+            numArr[2] = 3;
+            numArr[3] = 4;
+            numArr[4] = 5;
+            foreach (var item in numArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
