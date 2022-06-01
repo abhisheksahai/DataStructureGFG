998e45e4a914c81016459c5ef65ee4d0b366ed43
Abhishek Sahai <abhishek.sahai@outlook.in>
2022-06-01 10:33:25 +0530

    Datastructure

---
+++ StackGFG/Program.cs
@@ -0,0 +1,26 @@
+﻿//LIFO : Last in first out
+Stack<int> intStack = new Stack<int>();
+intStack.Push(0);
+intStack.Push(1);
+intStack.Push(2);
+intStack.Push(4);
+intStack.Push(5);
+
+Console.WriteLine("Stack");
+int count= intStack.Count;
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{intStack.Pop()} ");
+}
+
+
+Queue<int> intQueue = new Queue<int>();
+intQueue.Enqueue(0);
+intQueue.Enqueue(1);
+intQueue.Enqueue(2);
+count=intQueue.Count;
+Console.WriteLine($"{Environment.NewLine}Queue");
+for (int i = 0; i < count; i++)
+{
+    Console.Write($"{intQueue.Dequeue()} ");
+}
